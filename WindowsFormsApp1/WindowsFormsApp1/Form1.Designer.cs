2c839c27cf36ae73cf21a45aa91516a2c32e0554
kentough3110 <120083240+kentough3110@users.noreply.github.com>
2023-04-17 12:46:50 +0700

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvteam = new System.Windows.Forms.DataGridView();
+            this.comboBoxTeam = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.teamDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findMatchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Buttonenter = new System.Windows.Forms.Button();
+            this.dgvmanager = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvteam)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvmanager)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvteam
+            // 
+            this.dgvteam.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvteam.BackgroundColor = System.Drawing.Color.PaleTurquoise;
+            this.dgvteam.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvteam.Location = new System.Drawing.Point(12, 56);
+            this.dgvteam.Name = "dgvteam";
+            this.dgvteam.Size = new System.Drawing.Size(775, 166);
+            this.dgvteam.TabIndex = 0;
+            this.dgvteam.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvplayer_CellContentClick);
+            // 
+            // comboBoxTeam
+            // 
+            this.comboBoxTeam.FormattingEnabled = true;
+            this.comboBoxTeam.Location = new System.Drawing.Point(12, 27);
+            this.comboBoxTeam.Name = "comboBoxTeam";
+            this.comboBoxTeam.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTeam.TabIndex = 1;
+            this.comboBoxTeam.SelectedIndexChanged += new System.EventHandler(this.comboBoxnationality_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.teamDataToolStripMenuItem,
+            this.findMatchToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // teamDataToolStripMenuItem
+            // 
+            this.teamDataToolStripMenuItem.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teamDataToolStripMenuItem.Name = "teamDataToolStripMenuItem";
+            this.teamDataToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.teamDataToolStripMenuItem.Text = "Team Data";
+            this.teamDataToolStripMenuItem.Click += new System.EventHandler(this.teamDataToolStripMenuItem_Click);
+            // 
+            // findMatchToolStripMenuItem
+            // 
+            this.findMatchToolStripMenuItem.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.findMatchToolStripMenuItem.Name = "findMatchToolStripMenuItem";
+            this.findMatchToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.findMatchToolStripMenuItem.Text = "Find match";
+            this.findMatchToolStripMenuItem.Click += new System.EventHandler(this.findMatchToolStripMenuItem_Click);
+            // 
+            // Buttonenter
+            // 
+            this.Buttonenter.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.Buttonenter.FlatAppearance.BorderSize = 0;
+            this.Buttonenter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Buttonenter.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Buttonenter.ForeColor = System.Drawing.Color.White;
+            this.Buttonenter.Location = new System.Drawing.Point(140, 27);
+            this.Buttonenter.Name = "Buttonenter";
+            this.Buttonenter.Size = new System.Drawing.Size(75, 23);
+            this.Buttonenter.TabIndex = 3;
+            this.Buttonenter.Text = "ENTER";
+            this.Buttonenter.UseVisualStyleBackColor = false;
+            this.Buttonenter.Click += new System.EventHandler(this.Buttonenter_Click);
+            // 
+            // dgvmanager
+            // 
+            this.dgvmanager.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvmanager.BackgroundColor = System.Drawing.Color.PaleTurquoise;
+            this.dgvmanager.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvmanager.Location = new System.Drawing.Point(11, 250);
+            this.dgvmanager.Name = "dgvmanager";
+            this.dgvmanager.Size = new System.Drawing.Size(776, 179);
+            this.dgvmanager.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvmanager);
+            this.Controls.Add(this.Buttonenter);
+            this.Controls.Add(this.comboBoxTeam);
+            this.Controls.Add(this.dgvteam);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvteam)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvmanager)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvteam;
+        private System.Windows.Forms.ComboBox comboBoxTeam;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem teamDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findMatchToolStripMenuItem;
+        private System.Windows.Forms.Button Buttonenter;
+        private System.Windows.Forms.DataGridView dgvmanager;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string connectionstring = "server = localhost; uid = root; pwd = ; database = premier_league";
+        MySqlConnection sqlconnect;
+        MySqlCommand sqlcommand;
+        MySqlDataAdapter sqldataadapter;
+        string sqlquery;
+        DataTable dtpemain = new DataTable();
+        DataTable dtmanager = new DataTable();
+        DataTable dtmatch = new DataTable();
+        DataTable dtfilter = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlconnect = new MySqlConnection(connectionstring);
+            sqlquery = "SELECT team_name as `Team name` , team_id as `team` from team t ;";
+            sqlcommand = new MySqlCommand(sqlquery, sqlconnect);
+            sqldataadapter = new MySqlDataAdapter(sqlcommand);
+            sqldataadapter.Fill(dtfilter);
+            comboBoxTeam.DataSource = dtfilter;
+            comboBoxTeam.ValueMember = "team";
+            comboBoxTeam.DisplayMember = "Team name";
+        }
+
+        private void dgvplayer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBoxnationality_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void teamDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dgvmanager.Visible = true;
+        }
+
+        private void Buttonenter_Click(object sender, EventArgs e)
+        {
+            if (dgvmanager.Visible == true)
+            {
+                string tampung = comboBoxTeam.Text;
+                sqlconnect = new MySqlConnection(connectionstring);
+                //connect.Open(); 
+                dtpemain.Rows.Clear();
+                dtpemain.Columns.Clear();
+                sqlquery = "SELECT p.player_name as `Player Name` , t.team_name as `Team` ,p.playing_pos as `Pos`, p.weight as `Weight` , p.height as `Height` from player p, team t where p.team_id = t.team_id and t.team_name = '" + tampung + "'";
+                sqlcommand = new MySqlCommand(sqlquery, sqlconnect);
+                sqldataadapter = new MySqlDataAdapter(sqlcommand);
+                sqldataadapter.Fill(dtpemain);
+                dgvteam.DataSource = dtpemain;
+
+                dtmanager.Rows.Clear();
+                dtmanager.Columns.Clear();
+                sqlquery = "SELECT t.team_name as `Team Name` , m.manager_name as `Manager Name`, m.birthdate as `Birthdate`, n.nation as `Nationality` from team t , manager m , nationality n where m.manager_id = t.manager_id and m.nationality_id = n.nationality_id and t.team_name = '" + tampung + "';";
+                sqlcommand = new MySqlCommand(sqlquery, sqlconnect);
+                sqldataadapter = new MySqlDataAdapter(sqlcommand);
+                sqldataadapter.Fill(dtmanager);
+                dgvmanager.DataSource = dtmanager;
+            }
+
+            if (dgvmanager.Visible == false)
+            {
+                string tampung = comboBoxTeam.Text;
+                dtmatch.Rows.Clear();
+                dtmatch.Columns.Clear();
+                sqlconnect = new MySqlConnection(connectionstring);
+                sqlquery = "SELECT t.team_name as `Team Home` , tm.team_name as `Away Team` , m.match_date as `Date` , m.goal_home as `Home Goal` , m.goal_away as `Away Goal` , r.referee_name as `Referee`\r\nfrom `match` m , referee r, team t , team tm where r.referee_id = m.referee_id and t.team_id = m.team_home and tm.team_id = m.team_away and t.team_name = '" + tampung + "'\r\nunion\r\nSELECT t.team_name as `Team Home` , tm.team_name as `Away Team` , m.match_date as `Date` , m.goal_home as `Home Goal` , m.goal_away as `Away Goal` , r.referee_name as `Referee`\r\nfrom `match` m , referee r, team t , team tm where r.referee_id = m.referee_id and t.team_id = m.team_home and tm.team_id = m.team_away and tm.team_name = '" + tampung + "';";
+                sqlcommand = new MySqlCommand(sqlquery, sqlconnect);
+                sqldataadapter = new MySqlDataAdapter(sqlcommand);
+                sqldataadapter.Fill(dtmatch);
+                dgvteam.DataSource = dtmatch;
+            }
+        }
+
+        private void findMatchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dgvmanager.Visible = false;
+        }
+    }
+}
